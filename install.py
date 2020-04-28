8f90bf77123c6055572f52fe76cee7a2574a12c7
Cameron Podd <podd@usc.edu>
2020-04-28 10:59:56 -0700

    Macos should be working...maybe linux too

--- install.py
+++ install.py
@@ -1,26 +1,74 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import sys
+import json
+import os
+from pathlib import Path
+
+WINDOWS = 'windows'
+LINUX = 'linux'
+MACOS = 'macos'
+UNIX = 'unix'
+FILES_DIR = 'files'
+CONFIG_DIR = 'config'
+HOME = str(Path.home())
+CWD = os.getcwd()
 
 def main():
-    os_type = sys.platform
-    if os_type.startswith('darwin'):
-        return macOS()
-    elif os_type.startswith('win32'):
-        return windows()
-    elif os_type.startswith('linux'):
-        return linux()
-    else:
-        return 1
+    with open('data.json') as f:
+        data = json.load(f)
+        os_type = sys.platform
+        platform_name = ''
+        fs_type = UNIX
+        if os_type.startswith('darwin'):
+            platform_name = MACOS
+        elif os_type.startswith('win32'):
+            platform_name = WINDOWS
+        elif os_type.startswith('linux'):
+            platform_name = LINUX
 
-def macOS():
+        if platform_name == WINDOWS:
+            fs_type = WINDOWS
+
+        files = data['files']['shared'] + data['files'][platform_name]
+        folders = data['folders']['shared'] + data['folders'][platform_name]
+        config = data['config']['shared'] + data['config'][platform_name]
+
+        if fs_type == UNIX:
+            return unix(files, folders, config)
+
+        return windows(files, folders, config)
+
+def unix(files, folders, config):
+    hiddenConfig = hidden(CONFIG_DIR, UNIX)
+    for f in files:
+        fullPath = os.path.join(CWD, FILES_DIR, f)
+        hf = hidden(f, UNIX)
+        homePath = os.path.join(HOME, hf)
+        if os.path.isfile(fullPath) and not os.path.isfile(homePath):
+            os.symlink(fullPath, homePath)
+    for folder in folders:
+        fullPath = os.path.join(CWD, folder)
+        hf = hidden(folder, UNIX)
+        homePath = os.path.join(HOME, hf)
+        if os.path.isdir(fullPath) and not os.path.isdir(homePath):
+            os.symlink(fullPath, homePath)
+    for c in config:
+        fullPath = os.path.join(CWD, CONFIG_DIR, c)
+        homePath = os.path.join(HOME, hiddenConfig, c)
+        if os.path.exists(fullPath) and not os.path.exists(homePath):
+            os.symlink(fullPath, homePath)
+    return 0
+
+def windows(files, folders, config):
     return 1
 
-def windows():
-    return 1
+def hidden(f, fs_type):
+    if fs_type == UNIX:
+        return "." + f
 
-def linux():
-    return 1
+    return "_" + f
 
 if __name__ == "__main__":
-    main()
+    exit_code = main()
+    sys.exit(exit_code)
